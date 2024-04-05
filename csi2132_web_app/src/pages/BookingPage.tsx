c391bbbdd1c14397dcdb483aea130862a9d78323
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2024-04-05 15:32:47 -0400

    Update for testing deployment

--- csi2132_web_app/src/pages/BookingPage.tsx
+++ csi2132_web_app/src/pages/BookingPage.tsx
@@ -38,66 +38,66 @@ const BookingPage = () => {
   };
 
   return (
-      <AnimationFadeIn>
-        <div>
-          <form className="inputForm" onSubmit={handleSubmit}>
-            <div className="container">
-              <label htmlFor="customerId">
-                <b>Customer ID</b>
-              </label>
-              <input
-                  className="inputBlank"
-                  type="number"
-                  placeholder="Enter Customer ID"
-                  name="customerId"
-                  value={customerId}
-                  onChange={(e) => setCustomerId(e.target.value)}
-                  min={0}
-                  required
-              />
+    <AnimationFadeIn>
+      <div>
+        <form className="inputForm" onSubmit={handleSubmit}>
+          <div className="container">
+            <label htmlFor="customerId">
+              <b>Customer ID</b>
+            </label>
+            <input
+              className="inputBlank"
+              type="number"
+              placeholder="Enter Customer ID"
+              name="customerId"
+              value={customerId}
+              onChange={(e) => setCustomerId(e.target.value)}
+              min={0}
+              required
+            />
 
-              <label htmlFor="bookingDate">
-                <b>Booking Date</b>
-              </label>
-              <input
-                  className="inputBlank"
-                  type="date"
-                  name="bookingDate"
-                  value={bookingDate}
-                  onChange={(e) => setBookingDate(e.target.value)}
-                  required
-              />
+            <label htmlFor="bookingDate">
+              <b>Booking Date</b>
+            </label>
+            <input
+              className="inputBlank"
+              type="date"
+              name="bookingDate"
+              value={bookingDate}
+              onChange={(e) => setBookingDate(e.target.value)}
+              required
+            />
 
-              <label htmlFor="bookingId">
-                <b>Booking ID</b>
-              </label>
-              <input
-                  className="inputBlank"
-                  type="number"
-                  placeholder="Enter Booking ID"
-                  name="bookingId"
-                  value={bookingId}
-                  onChange={(e) => setBookingId(e.target.value)}
-                  min={0}
-                  required
-              />
+            <label htmlFor="bookingId">
+              <b>Booking ID</b>
+            </label>
+            <input
+              className="inputBlank"
+              type="number"
+              placeholder="Enter Booking ID"
+              name="bookingId"
+              value={bookingId}
+              onChange={(e) => setBookingId(e.target.value)}
+              min={0}
+              required
+            />
 
-              <button type="submit">Book Room</button>
-            </div>
-          </form>
+            <button type="submit">Book Room</button>
+          </div>
+        </form>
 
-          <p>
-            Room ID: {location.state.room_id}, Capacity: {location.state.capacity}
-            , Room Number: {location.state.room_number}, Hotel Address:{" "}
-            {location.state.hotel_address}, Price: {location.state.price},
-            Amenities: {location.state.amenities}, View Type:{" "}
-            {location.state.view_type}, Can Extend:{" "}
-            {location.state.can_extend ? "Yes" : "No"}
-          </p>
-        </div>
-      </AnimationFadeIn>
+        <p>
+          Room ID: {location.state.room_id}, Capacity: {location.state.capacity}
+          , Room Number: {location.state.room_number}, Hotel Address:{" "}
+          {location.state.hotel_address}, Price: {location.state.price},
+          Amenities: {location.state.amenities}, View Type:{" "}
+          {location.state.view_type}, Can Extend:{" "}
+          {location.state.can_extend ? "Yes" : "No"}
+        </p>
+        <div className="testing"></div>
+      </div>
+    </AnimationFadeIn>
   );
 };
 
 export default BookingPage;
-
